4135ce9f0120b436b56645a325feacc7accb26ab
skbarbee <barbee.s.k@gmail.com>
2022-10-04 09:55:09 -0400

    added all the characters

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ class Character {
 }
 
 const callieTorres = new Character("Calliope Iphengenia Torres", true, ["Callie","Callie O'Malley", "Ortho Goddess","Dr.T"], "Orthopedic Surgeon" )
-console.log(callieTorres)
 
+const meredithGrey = new Character("Meredith Gray", true, ["Chief of Surgery","Mer","Big Grey", "Our Lady of General Surgery","Mer-Der"], "General Surgery" )
+
+const alexKarev = new Character("Alexander Michael Karev", true, ["Alex","Dr. Hottie", "Boy Wonder","Dr. Doucheface"], "Pediatric Surgery" )
+
+const cristinaYang = new Character("Cristina Yang", true, ["Cristine","Twisted Sister", "Single Malt Scotch","Cardio God"], "Cardiothoracic Surgery" )
+
+const derekShepherd = new Character("Derek Christopher Shepherd", false, ["McDreamy","Great God of Neurosurgery", "McAss","Mer-Der"], "NeuroSurgery" )
+
+const markSloan = new Character("Mark Everett Sloan", false, ["McSteamy","Super famous plastics guy", "Pastics Posse","Chief Sloan"], "Plastic Surgery" )
+
+console.log(meredithGrey,callieTorres,alexKarev,cristinaYang,derekShepherd,markSloan)
 // console.log (cardArea)
 // const makeCards = ()=>{
 //     for (let i=0; i<characters.length; i++){
